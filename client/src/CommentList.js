292163bf9c48b1b50bc7c16cdafcab5111017c23
JEFFERSON JAVIER PAZMINO MENDOZA <jjpazminom@telconet.ec>
2024-03-02 18:09:55 -0500

    refactor(client react app): change get posts and commentsByPosts to the new endpoint to get all posts with its comments in one query created in our new microservice query; here we are using services for post and get independently

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,19 +1,5 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 
-
-const CommentList = ({ postId }) => {
-
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const response = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-    setComments(response.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-  }, []);
+const CommentList = ({ comments }) => {
 
   const renderedComments = comments.map(comment => {
     return <li key={comment.id}>{comment.content}</li>;
@@ -24,3 +10,32 @@ const CommentList = ({ postId }) => {
 
 
 export default CommentList;
+
+
+//! fetch comments is no longer needed because we are passing the comments as a prop from PostList because we are using Query microservice that provides the all the comments for each post
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+
+// const CommentList = ({ postId }) => {
+
+//   const [comments, setComments] = useState([]);
+
+//   const fetchComments = async () => {
+//     const response = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
+//     setComments(response.data);
+//   };
+
+//   useEffect(() => {
+//     fetchComments();
+//   }, []);
+
+//   const renderedComments = comments.map(comment => {
+//     return <li key={comment.id}>{comment.content}</li>;
+//   });
+
+//   return <ul>{renderedComments}</ul>;
+// }
+
+
+// export default CommentList;
